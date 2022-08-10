01666e8c60eeebed277e1fe04c74ea616fe769e0
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-10 15:47:21 +0100

    added js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-//Date and Time
+// Date & Time
+
 let now = new Date();
-let hourMinute = document.querySelector("#hour-minute");
-let dayMonthYear = document.querySelector("#day-month-year");
+let dateTime = document.querySelector("#date-time");
 let hour = now.getHours();
 let minute = now.getMinutes();
-if (minute > 10) {
+if (minute < 10) {
     minute = `0${minute}`;
 }
-
-let day = days[now.getDay()];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
 let date = now.getDate();
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-let month = months[now.getMonth];
-let year = now.getFullYear;
-hourMinute.innerHTML = `Last updated at ${hour}h ${minute}min`;
+let months = ["January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "Junr",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+dateTime.innerHTML = `Last updated at ${hour}:${minute}, on ${day}, ${month} ${date}, ${year}`;
